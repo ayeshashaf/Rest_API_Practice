2481219a34e4fd796951986ae3d00be0bd5cabe0
Ayesha Shafique <ayeshashafiqueshafique555@gmail.com>
2025-06-12 01:53:00 -0700

    first commit

--- lib/examples_rest_api/example1_rest_api.dart
+++ lib/examples_rest_api/example1_rest_api.dart
@@ -1,10 +1,102 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '/Model/PostsModel.dart';
 
 class Example1RestApi extends StatelessWidget {
-  const Example1RestApi({super.key});
+  Example1RestApi({super.key});
+
+  List<PostsModel> postList = [];
+
+  Future<List<PostsModel>> getPostsApi() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostsModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Rest API', style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Colors.teal,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostsApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return Text('Loading');
+                } else {
+                  return ListView.builder(
+                    itemCount: postList.length,
+                    itemBuilder: (context, index) {
+                      return Card(
+
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            spacing: 5,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                            Row(
+                              spacing: 12,
+                              children: [
+                                Text(
+                                  'User_Id :',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                ),
+                                Text(postList[index].userId.toString()),
+                              ],
+                            ),
+                              Row(
+                                spacing: 12,
+                                children: [
+                                  Text(
+                                    'Id :',
+                                    style: TextStyle(fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(postList[index].id.toString()),
+                                ],
+                              ),
+
+                              Text(
+                                'Title :',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                              Text(postList[index].title.toString()),
+                              Text(
+                                'Description :',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                              Text(postList[index].body.toString()),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
