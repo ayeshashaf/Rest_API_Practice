2d737ff689d3d54fa035c050f83613d37ff53031
Ayesha Shafique <ayeshashafiqueshafique555@gmail.com>
2025-06-12 00:36:37 -0700

    first commit

---
+++ lib/examples_rest_api/example1_rest_api.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Example1RestApi extends StatelessWidget {
+  const Example1RestApi({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
